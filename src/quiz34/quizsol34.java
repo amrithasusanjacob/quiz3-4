3f759806f17df19031aa7310c08398fb990961d9
753813 <753813@123-PC07.student.Tor.Loc>
2019-05-28 16:40:23 -0400

    Code added to show the number of times c appears and the number of times p appears in the code

--- src/quiz34/quizsol34.java
+++ src/quiz34/quizsol34.java
@@ -11,6 +11,21 @@ public class quizsol34 {
 		System.out.println("Length of the sentence is : "+l);
 		//last 15 characters of the string
 		System.out.println("Last 15 characters are :"+ s.substring(l-15, l));
+		//number of times c and p appears in the sentence 
+		int pn=0,cn=0;
+		for(int i=0;i<l;i++)
+		{
+			if(s.charAt(i)=='c')
+			{
+				cn=cn+1;
+			}
+			else if(s.charAt(i)=='p')
+			{
+				pn=pn+1;
+			}
+		}
+		System.out.println("Number of times c appears : "+cn);
+		System.out.println("Number of times p appears : "+pn);
 	}
 
 }
