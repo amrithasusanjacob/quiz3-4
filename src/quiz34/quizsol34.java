ac24e0fac665ffb0c17068615667403a384547ea
753813 <753813@123-PC07.student.Tor.Loc>
2019-05-28 16:29:44 -0400

    Added code for printing last 15 characters of the sentence

--- src/quiz34/quizsol34.java
+++ src/quiz34/quizsol34.java
@@ -9,6 +9,8 @@ public class quizsol34 {
 		//Number of characters
 		int l = s.length();
 		System.out.println("Length of the sentence is : "+l);
+		//last 15 characters of the string
+		System.out.println("Last 15 characters are :"+ s.substring(l-15, l));
 	}
 
 }
