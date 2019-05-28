cb0e32316a4d2b11648ca797d0a6ae4689f83080
753813 <753813@123-PC07.student.Tor.Loc>
2019-05-28 16:23:44 -0400

    Number of characters in the sentence

--- src/quiz34/quizsol34.java
+++ src/quiz34/quizsol34.java
@@ -5,6 +5,10 @@ public class quizsol34 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String s = "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+		//Number of characters
+		int l = s.length();
+		System.out.println("Length of the sentence is : "+l);
 	}
 
 }
